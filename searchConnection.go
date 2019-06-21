40d73fa0e32bd3d51e51e5886c41f94796e45df4
rudev <rudev@tutu.ru>
2019-06-21 11:49:35 +0300

    USA-1433 searchConnection frule added

---
+++ searchConnection.go
@@ -0,0 +1,199 @@
+package frule_module
+
+import (
+	"github.com/elliotchance/phpserialize"
+	"regexp"
+	"stash.tutu.ru/golang/resources/db"
+	"strconv"
+	"time"
+)
+
+type SearchConnectionRule struct {
+	Id                     int     `gorm:"column:id"`
+	Partner                *string `gorm:"column:partner"`
+	ConnectionGroup        *string `gorm:"column:connection_group"`
+	DepartureDate          time.Time
+	MinDepartureDate       *string `gorm:"column:min_departure_date"`
+	MinDepartureDateParsed []searchConnectionCronSpec
+	MaxDepartureDate       *string `gorm:"column:max_departure_date"`
+	MaxDepartureDateParsed []searchConnectionCronSpec
+	db                     *db.Database
+	specIntervalRegexp     *regexp.Regexp
+}
+
+type searchConnectionCronSpec struct {
+	spec  string
+	value string
+}
+
+func NewSearchConnectionFRule(db *db.Database) SearchConnectionRule {
+	return SearchConnectionRule{
+		db:                 db,
+		specIntervalRegexp: regexp.MustCompile(`\+(\d+)([dwmy])`),
+	}
+}
+
+func (sc SearchConnectionRule) GetResultValue(testRule interface{}) interface{} {
+	nowLocal := time.Now()
+	nowUtc := time.Now().UTC()
+
+	if minDepartureDateBorder := sc.getSpecInterval(sc.MinDepartureDateParsed, nowLocal); minDepartureDateBorder != "" {
+		if minDepartureDate := sc.getDateToCompare(minDepartureDateBorder, nowUtc); minDepartureDate != nil {
+			if !testRule.(SearchConnectionRule).DepartureDate.After(*minDepartureDate) {
+				return false
+			}
+		}
+
+	}
+
+	if maxDepartureDateBorder := sc.getSpecInterval(sc.MaxDepartureDateParsed, nowLocal); maxDepartureDateBorder != "" {
+		if maxDepartureDate := sc.getDateToCompare(maxDepartureDateBorder, nowUtc); maxDepartureDate != nil {
+			if !testRule.(SearchConnectionRule).DepartureDate.Before(*maxDepartureDate) {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func (sc SearchConnectionRule) GetComparisonOrder() ComparisonOrder {
+	return ComparisonOrder{
+		[]string{"partner", "connection_group"},
+	}
+}
+
+func (sc SearchConnectionRule) GetComparisonOperators() ComparisonOperators {
+	return ComparisonOperators{}
+}
+
+func (sc SearchConnectionRule) getStrategyKeys() []string {
+	return []string{"partner", "connection_group"}
+}
+
+func (sc SearchConnectionRule) getTableName() string {
+	return "rm_frule_search_connection"
+}
+
+func (sc SearchConnectionRule) GetDefaultValue() interface{} {
+	return false
+}
+
+func (sc SearchConnectionRule) GetLastUpdateTime() time.Time {
+	return getLastUpdateTime("search_connection", sc.db)
+}
+
+func (sc SearchConnectionRule) GetDataStorage() (map[int][]FRuler, error) {
+	result := make(map[int][]FRuler)
+
+	for rank, fieldList := range sc.GetComparisonOrder() {
+		query := sc.db.Table(sc.getTableName())
+		for _, field := range sc.getStrategyKeys() {
+			if inSlice(field, fieldList) {
+				query = query.Where(field + " IS NOT NULL")
+			} else {
+				query = query.Where(field + " IS NULL")
+			}
+		}
+		rows, err := query.Rows()
+		if err != nil {
+			return nil, err
+		}
+
+		for rows.Next() {
+			var rowData SearchConnectionRule
+
+			if err := sc.db.ScanRows(rows, &rowData); err != nil {
+				return nil, err
+			}
+
+			rowData.MinDepartureDateParsed, err = sc.parseCronSpecField(*rowData.MinDepartureDate)
+			if err != nil {
+				return nil, err
+			}
+
+			rowData.MaxDepartureDateParsed, err = sc.parseCronSpecField(*rowData.MaxDepartureDate)
+			if err != nil {
+				return nil, err
+			}
+
+			result[rank] = append(result[rank], rowData)
+
+		}
+		err = rows.Close()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return result, nil
+}
+
+func (sc SearchConnectionRule) parseCronSpecField(value string) ([]searchConnectionCronSpec, error) {
+	var unserialized map[interface{}]interface{}
+
+	err := phpserialize.Unmarshal([]byte(value), &unserialized)
+	if err != nil {
+		return nil, err
+	}
+
+	var resultParsed []searchConnectionCronSpec
+
+	for key, value := range unserialized {
+		var val string
+		switch value.(type) {
+		case string:
+			val = value.(string)
+			if val == "0" {
+				val = ""
+			}
+		case int64:
+			val = ""
+		}
+
+		resultParsed = append(resultParsed, searchConnectionCronSpec{key.(string), val})
+	}
+
+	return resultParsed, nil
+}
+
+func (sc SearchConnectionRule) getSpecInterval(specs []searchConnectionCronSpec, t time.Time) string {
+	for i := range specs {
+		if cronSpec(&specs[i].spec, t) {
+			return specs[i].value
+		}
+	}
+	return ""
+}
+
+func (sc SearchConnectionRule) getDateToCompare(intervalString string, t time.Time) *time.Time {
+	intervalParsed := sc.specIntervalRegexp.FindStringSubmatch(intervalString)
+
+	if len(intervalParsed) >= 3 {
+		intervalInt, err := strconv.Atoi(intervalParsed[1])
+		if err != nil {
+			return nil
+		}
+
+		var years, months, days int
+
+		switch intervalParsed[2] {
+		case "d":
+			days = intervalInt
+		case "w":
+			days = intervalInt * 7
+		case "m":
+			months = intervalInt
+		case "y":
+			years = intervalInt
+		default:
+			return nil
+		}
+
+		minDepartureDate := t.AddDate(years, months, days)
+
+		return &minDepartureDate
+	}
+
+	return nil
+}
---
+++ searchConnection_test.go
@@ -0,0 +1,98 @@
+package frule_module
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"runtime"
+	"stash.tutu.ru/golang/resources/db"
+	"stash.tutu.ru/golang/resources/db/mysql"
+	"strconv"
+	"testing"
+	"time"
+)
+
+func TestSearchConnectionDb(t *testing.T) {
+	database := mysql.NewDb()
+	database.WithConfig(db.Config{
+		DSN:   "webuser:qazxswedc@tcp(devel-02.mysql.avia.devel.tutu.ru:3306)/devel",
+		Debug: true,
+	})
+	err := database.Init()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	partner := "new_tt"
+	connectionGroup := "galileo"
+	testRule := SearchConnectionRule{
+		Partner:         &partner,
+		ConnectionGroup: &connectionGroup,
+		DepartureDate:   time.Now(),
+	}
+
+	ctx := context.Background()
+	rule := NewFRule(ctx, NewSearchConnectionFRule(database))
+
+	result := rule.GetResult(testRule).(bool)
+	fmt.Println(result)
+	//assert.True(t, result)
+}
+
+func TestSearchConnectionSpecs(t *testing.T) {
+	database := mysql.NewDb()
+	rule := NewSearchConnectionFRule(database)
+
+	specs := []searchConnectionCronSpec{
+		{"50-59 23 * * *", "+3w"},
+		{"0-15 20 * * *", "+100d"},
+		{"16-30 20 * * *", "+1y"},
+		{"* 0-19 * * *", "+2m"},
+		{"* * * * *", "0"},
+	}
+
+	now, _ := time.Parse("2006-01-02 15:04:05", "2019-06-21 11:21:00")
+
+	compareList := []compareStructure{
+		{getLine(), rule.getSpecInterval(specs, parseTime("2019-10-10 01:00:00")), "+2m"},
+		{getLine(), rule.getSpecInterval(specs, parseTime("2019-10-10 19:01:00")), "+2m"},
+		{getLine(), rule.getSpecInterval(specs, parseTime("2019-10-10 20:17:00")), "+1y"},
+		{getLine(), rule.getSpecInterval(specs, parseTime("2019-10-10 20:05:00")), "+100d"},
+		{getLine(), rule.getSpecInterval(specs, parseTime("2019-10-10 23:50:00")), "+3w"},
+
+		{getLine(), rule.getDateToCompare(rule.getSpecInterval(specs, parseTime("2019-10-10 01:00:00")), now).Format("2006-01-02"), "2019-08-21"},
+		{getLine(), rule.getDateToCompare(rule.getSpecInterval(specs, parseTime("2019-10-10 19:01:00")), now).Format("2006-01-02"), "2019-08-21"},
+		{getLine(), rule.getDateToCompare(rule.getSpecInterval(specs, parseTime("2019-10-10 20:17:00")), now).Format("2006-01-02"), "2020-06-21"},
+		{getLine(), rule.getDateToCompare(rule.getSpecInterval(specs, parseTime("2019-10-10 20:05:00")), now).Format("2006-01-02"), "2019-09-29"},
+		{getLine(), rule.getDateToCompare(rule.getSpecInterval(specs, parseTime("2019-10-10 23:50:00")), now).Format("2006-01-02"), "2019-07-12"},
+	}
+
+	compare(t, compareList)
+}
+
+func parseTime(s string) time.Time {
+	t, _ := time.Parse("2006-01-02 15:04:05", s)
+	return t
+}
+
+func getLine() int {
+	_, _, l, _ := runtime.Caller(1)
+	return l
+}
+
+type compareStructure struct {
+	line int
+	got  interface{}
+	want interface{}
+}
+
+func compare(t *testing.T, compareList []compareStructure) {
+	for i := range compareList {
+		t.Run("Line"+strconv.Itoa(compareList[i].line),
+			func(t *testing.T) {
+				if !reflect.DeepEqual(compareList[i].got, compareList[i].want) {
+					t.Errorf("got '%v' want '%v' structure on the %d line", compareList[i].got, compareList[i].want, compareList[i].line)
+				}
+			})
+	}
+}
