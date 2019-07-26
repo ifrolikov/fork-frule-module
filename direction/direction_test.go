7bf1b46e851729c9d40934d33c640ab1d8b73d9c
rudev <rudev@tutu.ru>
2019-07-26 19:05:18 +0300

    Direction tests added

--- direction/direction_test.go
+++ direction/direction_test.go
@@ -9,11 +9,9 @@ import (
 	"testing"
 )
 
-func TestDirection(t *testing.T) {
-	pwd, _ := filepath.Abs("./")
-	testConfig := &repository.Config{
-		DataURI: "file://" + pwd + "/../testdata/direction.json",
-	}
+func TestDirectionStorage(t *testing.T) {
+	pwd, _ := filepath.Abs("../")
+	testConfig := &repository.Config{DataURI: filepath.ToSlash("file://" + pwd + "/testdata/direction.json")}
 	ctx := context.Background()
 	defer ctx.Done()
 
@@ -34,4 +32,57 @@ func TestDirection(t *testing.T) {
 		}
 	}
 	assert.Equal(t, maxKey, 7)
+}
+
+func TestDirectionData(t *testing.T) {
+	pwd, _ := filepath.Abs("../")
+	testConfig := &repository.Config{DataURI: filepath.ToSlash("file://" + pwd + "/testdata/direction.json")}
+	ctx := context.Background()
+	defer ctx.Done()
+
+	directionFRule, err := NewDirectionFRule(ctx, testConfig)
+	assert.Nil(t, err)
+
+	frule := frule_module.NewFRule(ctx, directionFRule)
+	assert.NotNil(t, frule)
+
+	partner := "new_tt"
+	assert.True(t, frule.GetResult(DirectionRule{Partner: &partner}).(bool))
+
+	connectionGroup := "galileo"
+	carrierId := int64(1106)
+	departureCountryId := uint64(7)
+	departureCityId := uint64(491)
+	arrivalCountryId := uint64(7)
+	arrivalCityId := uint64(10105)
+
+	assert.True(t,
+		frule.GetResult(
+			DirectionRule{
+				Partner:            &partner,
+				ConnectionGroup:    &connectionGroup,
+				CarrierId:          &carrierId,
+				DepartureCountryId: &departureCountryId,
+				DepartureCityId:    &departureCityId,
+				ArrivalCountryId:   &arrivalCountryId,
+				ArrivalCityId:      &arrivalCityId,
+			}).(bool))
+
+	partner = "tt"
+	assert.False(t, frule.GetResult(DirectionRule{Partner: &partner}).(bool))
+
+	assert.False(t,
+		frule.GetResult(
+			DirectionRule{
+				Partner:            &partner,
+				ConnectionGroup:    &connectionGroup,
+				CarrierId:          &carrierId,
+				DepartureCountryId: &departureCountryId,
+				DepartureCityId:    &departureCityId,
+				ArrivalCountryId:   &arrivalCountryId,
+				ArrivalCityId:      &arrivalCityId,
+			}).(bool))
+
+	partner = "unknown"
+	assert.Equal(t, directionFRule.GetDefaultValue(), frule.GetResult(DirectionRule{Partner: &partner}).(bool))
 }
